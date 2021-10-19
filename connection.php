4dbe5669fc40b5346f517dbe5261f927b93b8fa1
BaoGCC19240 <91667147+BaoGCC19240@users.noreply.github.com>
2021-10-19 16:49:40 +0700

    chang commonline connect to postgre

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
 <?php
-    $conn = mysqli_connect('localhost','root','','shopping')
-    or die("Can not connect database".mysqli_connect_error());
+   $Connect = pg_connect("postgres://rjomnyfarzimda:dac4f698ee9371ce1a7f78bac394062a2f4138ef16a4971c0661029ac7006708@ec2-3-222-24-200.compute-1.amazonaws.com:5432/dct24likocdc2j");
+    if (!$Connect) {
+        die("Connection failed");
 ?>
